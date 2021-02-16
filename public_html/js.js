e3565d31bc8d1b80faff7a20c1a7d481eed3c44c
Komonyi <komonyi1996@gmail.com>
2021-02-16 10:00:18 +0100

    Kártyák működése

--- public_html/js.js
+++ public_html/js.js
@@ -1,8 +1,39 @@
+
+
 $(function () {
-init();
-   // $("#kezd").click(init);
+    init();
+    // $("#kezd").click(init);
 });
 function init() {
-    
-    
+
+    for (var i = 0; i < 32; i++) {
+        $("#jatekter").append("<div>");
+
+        $("#jatekter div").eq(i).append("<img>");
+        $("#jatekter div").eq(i).attr({"id": (i + 1)});
+
+        // $("#jatekter img").eq(i).attr({"src": fajlnev, "alt": "Kártyalap"});
+        $("#jatekter img").eq(i).attr({"src": "kartya/hatlap.jpg", "alt": "Kártyalap"});
+
+    }
+
+    $("#jatekter div").click(lapcsere);
+    $("#jatekter div").hover(function () {
+        $(this).addClass("kartya");
+        console.log("fölötte");
+
+    },
+            function () {
+                $(this).removeClass("kartya");
+                      console.log("lejött");
+            }
+    );
+
+}
+
+
+function lapcsere() {
+    var i = $(this).attr("id");
+    var fajlnev = "kartya/" + i + ".png";
+    $("#jatekter img").eq(i - 1).attr({"src": fajlnev, "alt": "Kártyalap"});
 }
